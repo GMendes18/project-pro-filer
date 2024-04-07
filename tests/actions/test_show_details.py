30fb5d0b6ca179a120112b142a3763e18dd38f33
Gabriel Mendes <gabri.mevial@gmail.com>
2024-04-07 14:18:05 -0300

    corrigindo req 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,16 +1,27 @@
-from pro_filer.actions.main_actions import show_details  # NOQA
-import os
+import pytest
+from pro_filer.actions.main_actions import show_details
 
 
-def test_show_details_file_exists(capsys):
-    context = {
-        "base_path": "images/pro-filer-preview.gif"
-    }
+@pytest.fixture
+def file_exists_context():
+    return {"base_path": "images/pro-filer-preview.gif"}
 
-    show_details(context)
+
+@pytest.fixture
+def file_not_exists_context():
+    return {"base_path": "/home/trybe/???"}
+
+
+@pytest.fixture
+def file_without_extension_context():
+    return {"base_path": "images"}
+
+
+def test_show_details_file_exists(capsys, file_exists_context):
+    show_details(file_exists_context)
     captured = capsys.readouterr()
     assert captured.out == (
-        f"File name: {os.path.basename(context['base_path'])}\n"
+        "File name: pro-filer-preview.gif\n"
         "File size in bytes: 270824\n"
         "File type: file\n"
         "File extension: .gif\n"
@@ -18,29 +29,22 @@ def test_show_details_file_exists(capsys):
     )
 
 
-def test_show_details_file_not_exists(capsys):
-    context = {
-        "base_path": "/home/trybe/???"
-    }
-
-    show_details(context)
+def test_show_details_file_not_exists(capsys, file_not_exists_context):
+    show_details(file_not_exists_context)
     captured = capsys.readouterr()
     assert captured.out == (
-        f"File '{os.path.basename(context['base_path'])}' does not exist\n"
+        "File '???' does not exist\n"
     )
 
 
-def test_show_details_file_without_extension(capsys):
-    context = {
-        "base_path": "images"
-    }
-
-    show_details(context)
+def test_show_details_file_without_extension(
+        capsys, file_without_extension_context):
+    show_details(file_without_extension_context)
     captured = capsys.readouterr()
     assert captured.out == (
-        f"File name: {os.path.basename(context['base_path'])}\n"
+        "File name: images\n"
         "File size in bytes: 4096\n"
         "File type: directory\n"
         "File extension: [no extension]\n"
-        f"Last modified date: 2024-03-28\n"
+        "Last modified date: 2024-03-28\n"
     )
