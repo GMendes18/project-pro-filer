f88572406044e9370efb334af78f2e99b16d58db
Gabriel Mendes <gabri.mevial@gmail.com>
2024-04-07 15:09:34 -0300

    requisito 4 com a data corrigida

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,15 +1,6 @@
 import pytest
-import os
 from datetime import date
 from pro_filer.actions.main_actions import show_details
-
-
-@pytest.fixture
-def file_exists_context(tmp_path):
-    # Criação de um arquivo dentro do diretório temporário
-    file_path = tmp_path / "pro-filer-preview.gif"
-    file_path.write_text("File content")
-    return {"base_path": str(file_path)}
 
 
 @pytest.fixture
@@ -17,20 +8,15 @@ def file_not_exists_context():
     return {"base_path": "/home/trybe/???"}
 
 
-@pytest.fixture
-def file_without_extension_context(tmp_path):
-    # Criação de um diretório dentro do diretório temporário
-    dir_path = tmp_path / "images"
-    os.mkdir(dir_path)
-    return {"base_path": str(dir_path)}
-
-
-def test_show_details_file_exists(capsys, file_exists_context):
-    show_details(file_exists_context)
+def test_show_details_file_exists(capsys, tmp_path):
+    file_path = tmp_path / "pro-filer-preview.gif"
+    file_path.write_text("File content")
+    file = {"base_path": str(file_path)}
+    show_details(file)
     captured = capsys.readouterr()
     assert captured.out == (
         "File name: pro-filer-preview.gif\n"
-        "File size in bytes: 12\n"  # Tamanho do conteúdo do arquivo
+        f"File size in bytes: {file_path.stat().st_size}\n"
         "File type: file\n"
         "File extension: .gif\n"
         f"Last modified date: {date.today()}\n"
@@ -46,13 +32,16 @@ def test_show_details_file_not_exists(capsys, file_not_exists_context):
 
 
 def test_show_details_file_without_extension(
-        capsys, file_without_extension_context):
-    show_details(file_without_extension_context)
+        capsys, tmp_path):
+    dir_path = tmp_path / "images"
+    dir_path.write_text("File content")
+    file = {"base_path": str(dir_path)}
+    show_details(file)
     captured = capsys.readouterr()
     assert captured.out == (
         "File name: images\n"
-        "File size in bytes: 4096\n"  # Tamanho de um diretório vazio
-        "File type: directory\n"
+        f"File size in bytes: {dir_path.stat().st_size}\n"
+        "File type: file\n"
         "File extension: [no extension]\n"
         f"Last modified date: {date.today()}\n"
     )
